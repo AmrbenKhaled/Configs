84d4c3c49e34cb00491daeb6ec0c2a93c4bd6640
AmrbenKhaled <148468490+AmrbenKhaled@users.noreply.github.com>
2023-10-26 21:57:53 +0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,2 @@
+sudo pacman -S xorg arandr git make unzip oh-my-zsh curl gimp inkscape kdenlive audacity htop mpv sxiv wget bluez bluez-tools alsa pipewire mypaint blueman zathura nnn  exa feh xinit zsh dmenu firefox code neovim bspwm sxhkd tmux alacritty 
+
