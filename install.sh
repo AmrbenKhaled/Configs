625473f55917c0f20e19335d995d51bf1cfaee75
AmrbenKhaled <148468490+AmrbenKhaled@users.noreply.github.com>
2023-10-27 20:51:44 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
-sudo pacman -S xorg git make unzip curl htop mpv sxiv wget bluez bluez-tools alsa-utils pipewire mypaint nnn exa feh xinit dmenu firefox code neovim i3 tmux alacritty 
+sudo pacman -S xorg git make unzip curl htop mpv sxiv wget bluez bluez-tools alsa-utils pipewire mypaint nnn exa feh xinit suckless-tools firefox-esr code neovim i3 tmux alacritty 
 
