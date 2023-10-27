31e572c0e0871e6119bac011cffccac6b88e1600
AmrbenKhaled <148468490+AmrbenKhaled@users.noreply.github.com>
2023-10-27 20:53:55 +0200

    Delete install.sh

--- install.sh
+++
@@ -1,6 +0,0 @@
-sudo pacman -S xorg git make unzip curl htop mpv sxiv wget bluez bluez-tools alsa-utils pipewire mypaint nnn exa feh xinit firefox-esr i3 tmux alacritty 
-
-## Build from source
-neovim
-VS code
-demnu
